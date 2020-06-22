eab6a90d4b70d17d8037e832e6a24b45d25b4b65
Kyle Schulz <kyle@schulz.com>
2020-06-22 18:26:57 -0500

    added exception example for code walkthrough

---
+++ src/main/java/com/aim/exceptions/MultipleCatchBlocks.java
@@ -0,0 +1,17 @@
+package com.aim.exceptions;
+
+public class MultipleCatchBlocks {
+
+    public static int[] arrayOfNumbers = new int[10];
+
+    public static void main(String[] args) {
+
+        System.out.println("\nThis program has a few errors in it and they need to be resolved:\n");
+
+        arrayOfNumbers[10] = Integer.parseInt(args[0]);
+        System.out.println("Element 10: " + arrayOfNumbers[10]);
+
+        System.out.println("\nGoodbye ...\n");
+
+    }
+}
